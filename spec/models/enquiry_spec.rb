ad64db5995f0e079b9bb838771744d3a5f5034d3
Mike Rogers <me@mikerogers.io>
2018-08-11 16:22:49 +0100

    Spec around create_from_source covering some edge cases which might occour

--- spec/models/enquiry_spec.rb
+++ spec/models/enquiry_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Enquiry, type: :model do
-  describe '#search(query)' do
+  describe '#search' do
     let!(:enquiry_1){ Enquiry.create!(name: 'Sarah-Jane Hardigan', email: 'sjhardigan@mailinator.com', message: 'Example Message', source_url: 'some-file.html') }
     let!(:enquiry_2){ Enquiry.create!(name: 'John Smith', email: 'johnsmith@mailinator.com', message: 'Example Message', source_url: 'some-file-2.html') }
 
@@ -16,8 +16,52 @@ RSpec.describe Enquiry, type: :model do
     end
   end
 
-  describe '.verify_email_quality!' do
+  describe '#create_from_source' do
+    let(:enquiry) { Enquiry.create_from_source( source: :a_m_direct, source_html: source_html) }
     pending
+
+    context 'given an blank source_html' do
+      let(:source_html) { '' }
+
+      it 'Will return an unsaved enquiry' do
+        expect(enquiry.persisted?).to eq(false)
+      end
+    end
+
+    context 'given an source_html with a duplicated listing_url' do
+      let(:source_html) { File.open('spec/fixtures/amdirect-1.html').read }
+
+      before do
+        Enquiry.create_from_source( source: :a_m_direct, source_html: source_html)
+      end
+
+      it 'Will return an unsaved enquiry' do
+        expect(Enquiry.count).to eq(1)
+        expect(Enquiry.first.lisiting_url).to eq(enquiry.listing_url)
+        expect(enquiry.persisted?).to eq(false)
+      end
+    end
+
+    context 'given an source_html with an invaid email' do
+      let(:source_html) { File.open('spec/fixtures/amdirect-1.html').read }
+
+      it 'Will return an saved Enquiry with the state "invalid"' do
+        expect(enquiry.persisted?).to eq(true)
+        expect(enquiry.state).to eq('invalid')
+      end
+    end
+
+    context 'given an source_html with an vaid email' do
+      let(:source_html) { File.open('spec/fixtures/amdirect-1.html').read }
+
+      it 'Will return an saved Enquiry with the state "open"' do
+        expect(enquiry.persisted?).to eq(true)
+        expect(enquiry.state).to eq('open')
+      end
+    end
+  end
+
+  describe '.verify_email_quality!' do
     let(:enquiry){ Enquiry.new(email: 'sjhardigan@mailinator.com') }
 
     context 'a valid email' do
