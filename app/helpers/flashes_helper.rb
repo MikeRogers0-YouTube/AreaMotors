7ef1281589023c58dda56d0b444d4d247b730fa6
Mike Rogers <me@mikerogers.io>
2018-08-11 19:04:22 +0100

    Reducing our Bootstrap requirements to just what we need and overriding the theme colours to fit in with Rails flashes a bit better

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -3,9 +3,9 @@ module FlashesHelper
   # to keep my views logic free.
   def render_flashes
     return unless flash.any?
-    content_tag :div, class: 'alerts container text-center border-bottom mt-4 mb-4' do
+    content_tag :div, class: 'alerts container mt-4' do
       flash.collect do |key, value|
-        content_tag :p, value, class: "alerts__alert alerts--#{key}"
+        content_tag :div, value, class: "alert alert-#{key}", role: :alert
       end.join('').html_safe
     end
   end
