fbd33de2a7f48c6974508d0bf7e9fa8413e63e1f
Mike Rogers <me@mikerogers.io>
2018-08-11 18:45:22 +0100

    Removing the pending notices from these last two controllers
    Flashes helper was tested as part of acceptance, enquiries just needed
    the generated CRUD removed.

--- spec/controllers/enquiries_controller_spec.rb
+++ spec/controllers/enquiries_controller_spec.rb
@@ -2,34 +2,20 @@ require 'rails_helper'
 
 RSpec.describe EnquiriesController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Enquiry. As you add validations to Enquiry, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # EnquiriesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let!(:enquiry) do
+    Enquiry.create_from_source( source: :a_m_direct, source_html: File.open('spec/fixtures/amdirect-1.html').read )
+  end
 
   describe "GET #index" do
     it "returns a success response" do
-      Enquiry.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      get :index, params: {}
       expect(response).to be_successful
     end
   end
 
   describe "GET #show" do
     it "returns a success response" do
-      enquiry = Enquiry.create! valid_attributes
-      get :show, params: {id: enquiry.to_param}, session: valid_session
+      get :show, params: { id: enquiry.to_param }
       expect(response).to be_successful
     end
   end
