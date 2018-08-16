41f79034cd85436ed4e30111ec17c0d9e02bca80
Mike Rogers <me@mikerogers.io>
2018-08-16 13:20:13 +0100

    Adding a show action for customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,4 +4,15 @@ class CustomersController < ApplicationController
 
   def show
   end
+
+  private
+  helper_method :resource
+  def resource
+    @resource ||= Customer.find(params[:id]).decorate
+  end
+
+  helper_method :collection
+  def collection
+    @collection ||= Customer.all.decorate
+  end
 end
